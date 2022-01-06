d9ba974304c7ab8d4b57be45a09bcaf7b43a459b
Sanjanasingh134 <sanjanasingh09oct@gmail.com>
2022-01-06 23:51:59 +0530

    modified-navigation

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Constants from 'expo-constants';
 
 // You can import from local files
 import HomeScreen from './screens/HomeScreen';
+import ThankingScreen from './screens/ThankingScreen';
 
 export default class App extends React.Component {
   render() {
@@ -18,7 +19,8 @@ export default class App extends React.Component {
 }
 
 var AppNavigator = createSwitchNavigator({
-  HomeScreen: HomeScreen
+  HomeScreen: HomeScreen,
+  ThankingScreen: ThankingScreen
 });
 
 const AppContainer = createAppContainer(AppNavigator);
