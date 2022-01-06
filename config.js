0007be285682b53687b09f11d755c0cd0be5e05e
Sanjanasingh134 <sanjanasingh09oct@gmail.com>
2022-01-06 23:12:16 +0530

    added_sdk_files

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
 
-};
+    var firebaseConfig = {
+        apiKey: "AIzaSyCbsTGj3H4C87lf0l2jYhUFlljzNNpvZXA",
+        authDomain: "project-67-86095.firebaseapp.com",
+        databaseURL: "https://project-67-86095-default-rtdb.firebaseio.com",
+        projectId: "project-67-86095",
+        storageBucket: "project-67-86095.appspot.com",
+        messagingSenderId: "873981820727",
+        appId: "1:873981820727:web:dd92e9c91658934bcae4b2"
+      };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
